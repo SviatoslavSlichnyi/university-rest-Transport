40a13f385dad031b5df144ea749ffea0e66d5aa7
SviatoslavSlichnyi <Mygithub1510>
2021-05-11 00:39:56 +0300

    fix: PublicTransport Id Strategy was not specified

--- src/main/java/university/lab/transport/entity/PublicTransport.java
+++ src/main/java/university/lab/transport/entity/PublicTransport.java
@@ -17,6 +17,7 @@ import java.util.Set;
 public class PublicTransport {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long publicTransportId;
 
     private String type;
